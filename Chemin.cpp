8dc1d9196b7544885b4a257da48922e8b765721f
elfritze <ericguillemette2@gmail.com>
2014-06-02 08:38:10 -0400

    Ajout de définitions pour la classe Chemin pour la compilation

--- Chemin.cpp
+++ Chemin.cpp
@@ -12,6 +12,76 @@
 namespace TP1
 {
 
-	//Mettez l'implémentation de vos méthodes ici.
+	//Constructeur par défaut
+	Chemin::Chemin()
+	{
+
+	}
+
+	//Destructeur
+	Chemin:: ~Chemin()
+	{
+		
+	}
+
+	//constructeur de copie
+	Chemin::Chemin(const Chemin&)
+	{
+
+	}
+
+	//Surcharge de l'opérateur =
+	const Chemin& Chemin::operator= (const Chemin& source)
+	{
+		return source;
+	}
+
+	//Ajoute le nom d'une pièce avec sa distance au début, spécifiés par nomPiece et distanceDuDebut,
+	//à un chemin. Il n'y a pas de vérification particulière de doublons à faire.
+	void Chemin::ajoutePiece(const std::string &nomPiece, int distanceDuDebut)
+	{
+
+	}
+
+	//Supprime une pièce d'un chemin, dont l'indice dans la liste chaînée,commençant par 1,
+	//est spécifié par numPiece. La méthode doit lancer une exception invalid_argument si l'indice est invalide.
+	void Chemin::retirePiece(int numPiece)
+	{
+
+	}
+
+	/**
+	 * \brief obtenir le nom de la pièce du début d'un chemin
+	 */
+	const std::string & Chemin::getNomPiece() const
+	{
+		const std::string &str = "";
+		return str;
+	}
+
+	/**
+	 * \brief obtenir la distance du début de la pièce au début d'un chemin
+	 */
+	int Chemin::getDistanceDuDebut() const
+	{
+		return 0;
+	}
+
+	//Retourne la taille d'un chemin.
+	int Chemin::tailleChemin() const
+	{
+		return 0;
+	}
+
+	//Affiche le chemin c en affichant, ligne par ligne, chaque nom de case suivi d'une espace puis de la distance du début.
+	//Par exemple :
+	//  PremierePiece 0
+	//  CaseACote 1
+	//  AutreCase 2
+	//  Blabla 3
+	void Chemin::afficheChemin() const
+	{
+
+	}
 
 }

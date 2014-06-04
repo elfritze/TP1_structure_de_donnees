e3c620d79009449e9abc7622dc7c52137cc8b44a
elfritze <ericguillemette2@gmail.com>
2014-06-04 13:34:00 -0400

    commentaires et peaufinage  sur principal.cpp

--- Labyrinthe.cpp
+++ Labyrinthe.cpp
@@ -221,7 +221,7 @@ void Labyrinthe::ajoutePassage(Couleur couleur, int i1, int j1, int i2, int j2)
 	piece1->piece.ajoutePorte(nouvellePorte);
 }
 
-	//TODO
+	
 	//Cette fonction doit ajouter la pièce p à un labyrinthe. Dans le cas où
 	//une pièce du labyrinthe porte déjà un même nom, la méthode ne doit
 	//rien faire (faire un simple return, sans générer d'exception).
@@ -255,7 +255,7 @@ void Labyrinthe::ajoutePassage(Couleur couleur, int i1, int j1, int i2, int j2)
 		return ;
 	}
 
-	//TODO
+	
 	//Méthode qui solutionnera le labyrinthe.
 	//Retourne le nombre de dépalcements requis pour qu'un joueur traverse le labyrinthe.
 	int Labyrinthe::solutionner(Couleur joueur)
@@ -282,28 +282,40 @@ void Labyrinthe::ajoutePassage(Couleur couleur, int i1, int j1, int i2, int j2)
 		pieces.enfilePiece(courant->getNom(),0);
 		courant->setParcourue(true);
 		do{
+			//On enlever la premiere piece
 			pieces.defilePiece(nomPiece,distance);
 			courant =  &trouvePiece(nomPiece)->piece;
+
+			//Si la piece dépilée est la fin du labyrinthe on a trouvé la sortie
 			if(courant->getNom() == arrivee->getNom())
 				done = true;
 
+			//On parcoure toutes les portes de la pièce défilée pour trouver les destinations possibles
 			for (int i = 1;i <= courant->getPortes().tailleListePortes(); i ++){
 
+				//Destination possible
 				temp  = courant->getPortes().elementAt(i).getDestination();
+
+				//On s'assure que la porte soit de la même couleur que le joueur et qu'elle n'a pas été parcourue.
 				if(!temp->getParcourue() && courant->getPortes().elementAt(i).getCouleur()==joueur)
 				{
 					temp->setParcourue(true);
+
+					//Si la porte est valide on ajoute sa piece à la file
 					pieces.enfilePiece(temp->getNom(),distance+1);
 				}
 
 			}
 
-			
+			// On parcoure toutes les pièces du labyrinthe à la recherche de portes qui mène à la pièce actuelle
 			for (NoeudListePieces * i = dernier->suivant; i != dernier; i = i->suivant )
 			{
+				//On parcoure chacune des portes de la piece
 				for (int j = 1;j <= i->piece.getPortes().tailleListePortes(); j ++){
 
 					temp  = i->piece.getPortes().elementAt(j).getDestination();
+
+					//On s'assure que la destionation n'est pas parcourue, que la piece est de la bonne couleur et que la piece liée à la porte est la pièce actuelle
 					if(i->piece.getPortes().elementAt(j).getCouleur()==joueur && temp==courant && !i->piece.getParcourue())
 					{
 						temp->setParcourue(true);
@@ -313,15 +325,15 @@ void Labyrinthe::ajoutePassage(Couleur couleur, int i1, int j1, int i2, int j2)
 				}
 			}
 			
-		}while(!pieces.estVideFile() && !done);
+		}while(!pieces.estVideFile() && !done); // On arrête si la file est vide (aucune solution) ou si la fin a été trouvée
 
 		if(done)
 			return distance;
 		else
-			return 0;
+			return 0; // On retourne 0 si aucune solution
 	}
 
-	//TODO
+	
 	//Méthode qui solutionnera le labyrinthe.
 	//Retourne le chemin pris par le joueur.
 	Chemin Labyrinthe::cheminLabyrinthe(Couleur joueur)
@@ -329,21 +341,24 @@ void Labyrinthe::ajoutePassage(Couleur couleur, int i1, int j1, int i2, int j2)
 		return Chemin();
 	}
 
-	//TODO
+	
 	//Appelle 4 fois la méthode solutionner pour trouver le joueur gagnant.
 	Couleur Labyrinthe::trouveGagnant()
 	{
+		//On appelle la méthode solutionner 4 fois pour obtenir les scores de chacun des joueurs
 		int nbR = solutionner(Rouge);
 		int nbV = solutionner(Vert);
 		int nbB = solutionner(Bleu);
 		int nbJ = solutionner(Jaune);
-		int min = 9999;
+		int min = 99999; // Plus petit nombre de déplacements
 
+		// Chaque ligne vérifie si le minimum est plus petit que le nombre de déplacements du joueur et qu'on a bien une solution (différent de 0)
 		nbR < min && nbR != 0 ? min= nbR : min = min;
 		nbV < min && nbV != 0 ? min= nbV : min = min;
 		nbB < min && nbB != 0 ? min= nbB : min = min;
 		nbJ < min && nbJ != 0 ? min= nbJ : min = min;
 
+		//On retourne le plus petit score, sinon on retourne une couleur vide
 		if(min == nbR)
 			return Rouge;
 		else if(min==nbV)
@@ -356,7 +371,7 @@ void Labyrinthe::ajoutePassage(Couleur couleur, int i1, int j1, int i2, int j2)
 			return Couleur();
 	}
 
-	//TODO
+	
 	//Méthode privée. Retourne l'adresse du noeud de la liste de pièces contenue dans le labyrinthe lab
 	//qui correspond à la pièce portant le nom nom, la méthode doit lancer une exception invalid_argument si le nom de
 	//la pièce est vide. La méthode doit lancer également une exception logic_error si la pièce est introuvable.
@@ -364,23 +379,32 @@ void Labyrinthe::ajoutePassage(Couleur couleur, int i1, int j1, int i2, int j2)
 	//et non l'adresse de la pièce.
 	Labyrinthe::NoeudListePieces *Labyrinthe::trouvePiece(std::string &nom) const 
 	{
+		//Si le nom est vide on lance une exception
+		if (nom == "")
+			throw std::invalid_argument("trouvePiece: Le nom passé en paramètre est vide");
+
 		//On parcour la liste pour vérifier si une piece porte un même nom
 		NoeudListePieces *courant = 0;
 		courant = dernier->suivant;
 
+		//Si le dernier est la piece recherchée
 		if(dernier->piece.getNom() == nom)
 			return dernier;
 
+		//On parcoure toutes les pièces à la recherche de la bonne.
 		while(courant!=dernier)
 		{
 			if(courant->piece.getNom() == nom)
 				return courant;
+			// On passe au suivant
 			courant=courant->suivant;
 		}
-		return new NoeudListePieces(Piece());
+
+		//Si la piece est introuvable on lance une exception
+		throw std::logic_error("trouvePiece: La pièce n'a pas été trouvée");
 	}
 
-	//TODO
+	
 	//Méthode privée, elle sert pour charger un labyrinthe. Ajuste le pointeur depart d'un labyrinthe
 	//pour qu'il contienne l'adresse de la pièce correspondant au nom spécifié par nom. Lancer une
 	//exception logic_error si aucune pièce du labyrinthe ne porte le nom nom.
@@ -389,7 +413,7 @@ void Labyrinthe::ajoutePassage(Couleur couleur, int i1, int j1, int i2, int j2)
 		depart = &trouvePiece(nom)->piece;
 	}
 
-	//TODO
+	
 	//Méthode privée, elle sert pour charger un labyrinthe. Ajuste le pointeur arrivé d'un labyrinthe
 	//pour qu'il contienne l'adresse de la pièce correspondant au nom spécifié par nom. Lancer une
 	//exception logic_error si aucune pièce du labyrinthe ne porte le nom nom.
@@ -398,12 +422,13 @@ void Labyrinthe::ajoutePassage(Couleur couleur, int i1, int j1, int i2, int j2)
 		arrivee = &trouvePiece(nom)->piece;
 	}
 
-
+	//Méthode appelée pour copier un labyrinthe (liste circulaire)
 	void Labyrinthe:: _copier(NoeudListePieces * sn)
 	{
 		try{
 			dernier = new NoeudListePieces(sn->piece);
 			NoeudListePieces * nouveau = dernier;
+			//On copie chaque noeud et on passe au suivant
 			for (NoeudListePieces * temp = sn->suivant; temp != sn; temp = temp->suivant )
 			{
 				nouveau->suivant = new NoeudListePieces(temp->piece);
@@ -419,9 +444,10 @@ void Labyrinthe::ajoutePassage(Couleur couleur, int i1, int j1, int i2, int j2)
 		}
 	}
 
-
+	//Méthode appelée pour supprimer un labyrinthe (liste circulaire)
 	void Labyrinthe:: _detruire()
 	{
+		//Si le labyrinthe n'est pas déjà vide
 		if(dernier !=0)
 		{
 			NoeudListePieces * courant = dernier->suivant;

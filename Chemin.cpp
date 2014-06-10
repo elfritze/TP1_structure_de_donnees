b6cac0e6cb7ea4506898e1354039830ebbfef4f0
mathieulecuyer <mathieulecuyer@users.noreply.github.com>
2014-06-09 21:59:34 -0400

    Doxygen pour Chemin.h et Chemin.cpp

--- Chemin.cpp
+++ Chemin.cpp
@@ -1,179 +1,224 @@
 /**
  * \file Chemin.cpp
  * \brief Ce fichier contient une implantation des méthodes de la classe Chemin
- * \author Étudiant 1, étudiant 2
+ * \author Éric Guillemette, Mathieu L'Écuyer
  * \version 0.1
  * \date juin 2014
- *
  */
 
 #include "Chemin.h"
 #include <iostream>
+
 using namespace std;
+
+/**
+ * \namespace TP1
+ *
+ * Espace de nommage regroupant les définitions du TP1.
+ */
 namespace TP1
 {
 
-	//Constructeur par défaut
-	Chemin::Chemin() : debut(0){}
-
-	//Destructeur
-	Chemin:: ~Chemin()
-	{
-		_detruire();
-	}
-
-	//constructeur de copie
-	Chemin::Chemin(const Chemin& source)
-	{
-		debut = source.debut;
-		if (debut!=0)
-		{
-			_copier(source.debut);
-		}
-	}
-
-	//Surcharge de l'opérateur =
-	const Chemin& Chemin::operator= (const Chemin& source)
-	{
-		if (debut!=0)
-			_detruire();
-
-		//On copie le début
-		debut = source.debut;
-		if(source.debut!=0)
-			_copier(source.debut); //On appelle la méthode copier pour copier le reste de la liste chainée à partir du début.
-
-		return (*this);
-	}
-
-	//Ajoute le nom d'une pièce avec sa distance au début, spécifiés par nomPiece et distanceDuDebut,
-	//à un chemin. Il n'y a pas de vérification particulière de doublons à faire.
-	void Chemin::ajoutePiece(const std::string &nomPiece, int distanceDuDebut)
-	{
-		NoeudChemin * nouveau = new NoeudChemin(nomPiece,distanceDuDebut);
-		NoeudChemin * courant = debut;
-		NoeudChemin * pred = 0;
-
-		// On parcoure la liste jusqu'à la fin
-		while(courant != 0)
-		{
-			pred = courant;
-			courant = courant->suivant;
-		}
-
-		//Si la liste est vide on insere au début
-		if(debut==0)
-			debut = nouveau;
-		else
-			pred->suivant = nouveau; // Sinon on ajoute à la fin
-	}
-
-	//Supprime une pièce d'un chemin, dont l'indice dans la liste chaînée,commençant par 1,
-	//est spécifié par numPiece. La méthode doit lancer une exception invalid_argument si l'indice est invalide.
-	void Chemin::retirePiece(int numPiece)
-	{
-		//Vérification
-		if(numPiece >= 1 && numPiece <= tailleChemin()){
-			int i = 1;
-			NoeudChemin * courant = debut;
-			NoeudChemin * precedent = courant;
-
-			//On loop n fois pour atteindre la piece désirée
-			while(i < numPiece)
-			{
-				precedent = courant;
-				courant = courant->suivant;
-				i++;
-			}
-
-			//Si le noeud désiré est le premier on réassigne le début au suivant du début.
-			if(debut==courant)
-					debut=courant->suivant;
-			else
-				precedent->suivant=courant->suivant;// Sinon on associe le suivant du noeud précédant le noeud désiré au noeud suivant du noeud désiré
-
-			//On se débarasse du noeud désiré
-			courant->suivant = 0;
-			delete courant;
-		}else
-			throw std::invalid_argument("L'indice est invalide");
-	}
-
-	/**
-	 * \brief obtenir le nom de la pièce du début d'un chemin
-	 */
-	const std::string & Chemin::getNomPiece() const
-	{
-		return debut->nomPiece;
-	}
-
-	/**
-	 * \brief obtenir la distance du début de la pièce au début d'un chemin
-	 */
-	int Chemin::getDistanceDuDebut() const
-	{
-		return debut->distanceDuDebut;
-	}
-
-	//Retourne la taille d'un chemin.
-	int Chemin::tailleChemin() const
-	{
-		int i = 0;
-		for (NoeudChemin* temp = debut;temp != 0;temp = temp->suivant)
-			i++;
-
-		return i;
-	}
-
-	//Affiche le chemin c en affichant, ligne par ligne, chaque nom de case suivi d'une espace puis de la distance du début.
-	//Par exemple :
-	//  PremierePiece 0
-	//  CaseACote 1
-	//  AutreCase 2
-	//  Blabla 3
-	void Chemin::afficheChemin() const
-	{
-		for (NoeudChemin* temp = debut;temp != 0;temp = temp->suivant)
-		{
-			cout << temp->nomPiece << " " << temp->distanceDuDebut ;
-		}
-	}
-
-
-	void Chemin:: _copier(NoeudChemin* sn)
-	{
-		try{
-
-			// copier le premier noeud
-			debut = new NoeudChemin(sn->nomPiece,sn->distanceDuDebut);
-
-			NoeudChemin* nouveau = debut;
-			for (NoeudChemin* temp = sn->suivant;temp != 0;temp = temp->suivant)
-			{
-				nouveau->suivant = new NoeudChemin(temp->nomPiece,temp->distanceDuDebut);
-				nouveau = nouveau->suivant;
-			}
-		}
-		catch(std::exception&)
-		{
-			_detruire();
-			throw;
-		}
-	}
-
-	void Chemin::_detruire()
-	{
-		NoeudChemin * courant = debut;
-		NoeudChemin * temp = 0;
-		while (courant!=0)
-		{
-			temp = courant->suivant;
-			delete courant;
-			courant = temp;
-		}
-		debut = 0;
-		delete debut;
-		
-	}
+/**
+ * \fn Chemin::Chemin()
+ */
+Chemin::Chemin() :
+      debut(0)
+{
 
 }
+
+/**
+ * \fn Chemin::~Chemin()
+ */
+Chemin::~Chemin()
+{
+   _detruire();
+}
+
+/**
+ * \fn Chemin::Chemin(const Chemin& source)
+ *
+ * \param[in] source : Un objet Chemin existant.
+ */
+Chemin::Chemin(const Chemin& source)
+{
+   debut = source.debut;
+   if (debut != 0)
+   {
+      _copier(source.debut);
+   }
+}
+
+/**
+ * \fn const Chemin& Chemin::operator=(const Chemin& source)
+ *
+ * \param[in] source : Un objet Chemin existant.
+ *
+ * \return L'objet courant contenant à présent les données de l'objet source.
+ */
+const Chemin& Chemin::operator=(const Chemin& source)
+{
+   if (debut != 0)
+      _detruire();
+
+   //On copie le début
+   debut = source.debut;
+   if (source.debut != 0)
+      _copier(source.debut); //On appelle la méthode copier pour copier le reste de la liste chainée à partir du début.
+
+   return (*this);
+}
+
+/**
+ * \fn void Chemin::ajoutePiece(const std::string &nomPiece, int distanceDuDebut)
+ *
+ * \param[in] nomPiece : le nom de la pièce à ajouter à la file.
+ * \param[in] distanceDuDebut : le nombre de déplacement depuis le début du labyrinthe.
+ */
+void Chemin::ajoutePiece(const std::string &nomPiece, int distanceDuDebut)
+{
+   NoeudChemin * nouveau = new NoeudChemin(nomPiece, distanceDuDebut);
+   NoeudChemin * courant = debut;
+   NoeudChemin * pred = 0;
+
+   // On parcoure la liste jusqu'à la fin
+   while (courant != 0)
+   {
+      pred = courant;
+      courant = courant->suivant;
+   }
+
+   //Si la liste est vide on insere au début
+   if (debut == 0)
+      debut = nouveau;
+   else
+      pred->suivant = nouveau; // Sinon on ajoute à la fin
+}
+
+/**
+ * \fn void Chemin::retirePiece(int numPiece)
+ *
+ * \param[in] numPiece : l'indice de la pièce à supprimer dans la chaîne.
+ */
+void Chemin::retirePiece(int numPiece)
+{
+   //Vérification
+   if (numPiece >= 1 && numPiece <= tailleChemin())
+   {
+      int i = 1;
+      NoeudChemin * courant = debut;
+      NoeudChemin * precedent = courant;
+
+      //On loop n fois pour atteindre la piece désirée
+      while (i < numPiece)
+      {
+         precedent = courant;
+         courant = courant->suivant;
+         i++;
+      }
+
+      //Si le noeud désiré est le premier on réassigne le début au suivant du début.
+      if (debut == courant)
+         debut = courant->suivant;
+      else
+         precedent->suivant = courant->suivant;	// Sinon on associe le suivant du noeud précédant le noeud désiré au noeud suivant du noeud désiré
+
+      //On se débarasse du noeud désiré
+      courant->suivant = 0;
+      delete courant;
+   }
+   else
+      throw std::invalid_argument("L'indice est invalide");
+}
+
+/**
+ * \fn const std::string & Chemin::getNomPiece() const
+ *
+ * \return Le nom de la pièce au début du chemin.
+ */
+const std::string & Chemin::getNomPiece() const
+{
+   return debut->nomPiece;
+}
+
+/**
+ * \fn int Chemin::getDistanceDuDebut() const
+ *
+ * \return La distance du début, soit de la pièce au début d'un chemin.
+ */
+int Chemin::getDistanceDuDebut() const
+{
+   return debut->distanceDuDebut;
+}
+
+/**
+ * \fn int Chemin::tailleChemin() const
+ *
+ * \return Un entier représentant la taille du chemin.
+ */
+int Chemin::tailleChemin() const
+{
+   int i = 0;
+   for (NoeudChemin* temp = debut; temp != 0; temp = temp->suivant)
+      i++;
+
+   return i;
+}
+
+/**
+ * \fn void Chemin::afficheChemin() const
+ */
+void Chemin::afficheChemin() const
+{
+   for (NoeudChemin* temp = debut; temp != 0; temp = temp->suivant)
+   {
+      cout << temp->nomPiece << " " << temp->distanceDuDebut;
+   }
+}
+
+/**
+ * \fn void Chemin::_copier(NoeudChemin* sn)
+ *
+ * \param[in] sn : Un pointeur sur le début du chemin.
+ */
+void Chemin::_copier(NoeudChemin* sn)
+{
+   try
+   {
+
+      // copier le premier noeud
+      debut = new NoeudChemin(sn->nomPiece, sn->distanceDuDebut);
+
+      NoeudChemin* nouveau = debut;
+      for (NoeudChemin* temp = sn->suivant; temp != 0; temp = temp->suivant)
+      {
+         nouveau->suivant = new NoeudChemin(temp->nomPiece, temp->distanceDuDebut);
+         nouveau = nouveau->suivant;
+      }
+   }
+   catch (std::exception&)
+   {
+      _detruire();
+      throw;
+   }
+}
+
+/**
+ * \fn void Chemin::_detruire()
+ */
+void Chemin::_detruire()
+{
+   NoeudChemin * courant = debut;
+   NoeudChemin * temp = 0;
+   while (courant != 0)
+   {
+      temp = courant->suivant;
+      delete courant;
+      courant = temp;
+   }
+   debut = 0;
+   delete debut;
+}
+
+} // namespace TP1
--- Chemin.h
+++ Chemin.h
@@ -4,8 +4,10 @@
  * \author Admin
  * \version 0.1
  * \date mai 2014
- * 
  */
+
+// Révision des commentaires avec balises Doxygen.
+// Fichier mis à jour par : Éric Guillemette, Mathieu L'Écuyer
 
 #ifndef CHEMIN_H_
 #define CHEMIN_H_
@@ -13,85 +15,149 @@
 #include <stdexcept>
 #include <string>
 
-
+/**
+ * \namespace TP1
+ *
+ * Espace de nommage regroupant les définitions du TP1.
+ */
 namespace TP1
 {
 
+/**
+ * \class Chemin
+ * \brief Cette classe sert à définir le chemin du labyrinthe.
+ *
+ * Un chemin est implémenté dans une liste simplement chaînée de noms de pièces avec leurs
+ * distances du début, c'est-à-dire le nombre de déplacements effectués depuis la case de départ.
+ */
 class Chemin
 {
 public:
-	//Constructeur par défaut
+   /**
+    * \brief Constructeur par défaut.
+    *
+    * \post Une instance de la classe Chemin est initialisée.
+    */
 	Chemin();
 
-	//Destructeur
+   /**
+    * \brief Destructeur.
+    *
+    * \post L'instance de Chemin est détruite.
+    */
 	virtual ~Chemin();
 
-	//constructeur de copie
+   /**
+    * \brief Constructeur de copie.
+    *
+    * \post Une copie profonde d'un objet Chemin existant est initialisée.
+    */
 	Chemin(const Chemin&);
 
-	//Surcharge de l'opérateur =
+   /**
+    * \brief Surcharge de l'opérateur d'assignation =
+    *
+    * \post Une copie de Chemin est retournée.
+    */
 	const Chemin& operator =(const Chemin& source);
 
-	//Ajoute le nom d'une pièce avec sa distance au début, spécifiés par nomPiece et distanceDuDebut,
-	//à un chemin. Il n'y a pas de vérification particulière de doublons à faire.
+	/**
+    * \brief Ajoute à un chemin le nom d'une pièce avec sa distance du début du labyrinthe.
+    *
+    * \post Le nom d'une pièce et sa distance du début sont ajoutés au chemin dans un noeud typique.
+    */
 	void ajoutePiece(const std::string &nomPiece, int distanceDuDebut);
 
-	//Supprime une pièce d'un chemin, dont l'indice dans la liste chaînée,commençant par 1,
-	//est spécifié par numPiece. La méthode doit lancer une exception invalid_argument si l'indice est invalide.
+   /**
+    * \brief Supprime une pièce d'un chemin à l'indice spécifié par numPiece.
+    *
+    * \pre L'indice de la pièce à supprimer commence par 1.
+    * \pre L'indice de la pièce à supprimer doit être plus petit que le nombre de pièces.
+    *
+    * \post La pièce présente à l'indice numPiece est supprimée du chemin.
+    *
+    * \exception invalid_argument : si l'indice est invalide.
+    */
 	void retirePiece(int numPiece);
 
-	/**
-	 * \brief obtenir le nom de la pièce du début d'un chemin
-	 */
+   /**
+    * \brief Cette méthode retourne le nom de la pièce au début d'un chemin.
+    *
+    * \post Le nom de la pièce est retourné.
+    */
 	const std::string & getNomPiece() const;
 
-	/**
-	 * \brief obtenir la distance du début de la pièce au début d'un chemin
-	 */
+   /**
+    * \brief Cette méthode retourne la distance du début, soit de la pièce au début d'un chemin.
+    *
+    * \post La distance du début est retournée.
+    */
 	int getDistanceDuDebut() const;
 
-	//Retourne la taille d'un chemin.
+   /**
+    * \brief Cette méthode retourne la taille d'un chemin.
+    *
+    * \post La taille d'un chemin est retournée.
+    */
 	int tailleChemin() const;
 
-	//Affiche le chemin c en affichant, ligne par ligne, chaque nom de case suivi d'une espace puis de la distance du début.
-	//Par exemple :
-	//  PremierePiece 0
-	//  CaseACote 1
-	//  AutreCase 2
-	//  Blabla 3
+	/**
+    * \brief Affiche le chemin, soit en affichant ligne par ligne chaque nom de case suivi
+    *        de la distance du début.
+    *
+    * \post Le chemin est affiché.
+    */
 	void afficheChemin() const;
 
 private:
-	class NoeudChemin //noeud typique d'une liste simplement chaînée
+   /**
+    * \class NoeudChemin
+    *
+    * \brief Classe interne représentant un noeud typique.
+    *
+    * Cette classe représente un noeud typique pour implémenter une liste simplement chaînée.
+    */
+	class NoeudChemin
 	{
-		public:
-			// Le nom d'une pièce du labyrinthe.
-			std::string nomPiece;
+   public:
+      std::string nomPiece; /*!< Le nom d'une pièce du labyrinthe. */
 
-			// Le nombre de déplacements qu'il est nécessaire d'effectuer depuis la pièce de départ pour se rendre
-			// à ce point dans le chemin (à cette pièce).
-			int distanceDuDebut;
+      int distanceDuDebut; /*!< Le nombre de déplacements qu'il est nécessaire d'effectuer
+                                depuis la pièce de départ pour se rendre à ce point dans le
+                                chemin (à cette pièce). */
 
-			// Le point suivant du chemin (pièce suivante), ou 0 (NULL) s'il s'agit de la fin du chemin.
-			NoeudChemin *suivant;
+      NoeudChemin *suivant; /*!< Le point suivant du chemin (pièce suivante), ou 0 (NULL)
+                                 s'il s'agit de la fin du chemin. */
 
-			//Constructeur prenant le nom d'une piece et la distance du début
-			explicit NoeudChemin(std::string nomPiece,int distanceDuDebut,NoeudChemin* next_ptr = 0) :
-			                     nomPiece(nomPiece), distanceDuDebut(distanceDuDebut), suivant(next_ptr) {}
-
+      /**
+       * \brief Constructeur de la classe NoeudChemin.
+       *
+       * \post Un noeud typique est initialisé.
+       */
+      explicit NoeudChemin(std::string nomPiece,int distanceDuDebut,NoeudChemin* next_ptr = 0) :
+                           nomPiece(nomPiece),distanceDuDebut(distanceDuDebut),suivant(next_ptr) {}
 	};
 
-	// Un chemin est défini comme un pointeur vers le premier noeud de la liste simplement chaînée.
-	// Un chemin vide est représenté directement par le pointeur 0 (NULL).
-	NoeudChemin *debut;
+	NoeudChemin *debut; /*!< Un pointeur vers le premier noeud de la liste simplement chaînée.
+	                         Un chemin vide est représenté directement par le pointeur 0 (NULL). */
 
-	//Detruire le chemin
-	void  _detruire();
+   // Méthodes privées
 
-	//Copier un chemin
-	void _copier( NoeudChemin *);
+   /**
+    * \brief Cette méthode privée sert à détruire un chemin.
+    *
+    * \post Le chemin est détruit.
+    */
+   void  _detruire();
+
+   /**
+    * \brief Cette méthode privée sert à copier.
+    *
+    * \post Une copie profonde est créée.
+    */
+	void _copier(NoeudChemin *);
 };
 
-}
+} // namespace TP1
 
 #endif /* CHEMIN_H_ */
